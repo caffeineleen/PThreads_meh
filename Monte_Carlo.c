83159b17776fea1922e3333497f15dd87b1e216e
Kathleen Elisabeth Smith <smithka19@up.edu>
2018-03-06 15:24:03 -0800

    has some thread stuff in sudoku

--- Monte_Carlo.c
+++ Monte_Carlo.c
@@ -2,59 +2,65 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
-#include "graphics.h"
-#include "winbgim.h"
-//#include <conio.h>
 #include <time.h>
+#include <math.h>
 
 //gcc -o mc Monte_Carlo.c
 
 //PUT DEFINITIONS HERE
+double numGeneratorX();
+double numGeneratorY();
+void pointGenerator();
+
+//GLOBAL VARIABLE
+int totalCount = 1;
+int insideCount = 0;
+int circleX = 100;
+int circleY = 100;
 
 //SQUARE IS 200X200
 
-int numGeneratorX(){
+double numGeneratorX(){
   srand(time(0));
-  int xCoor = rand() % 200;
+  double xCoor = rand() % 200;
   return xCoor;
 }
 
-int numGeneratorY(){
-    int yCoor = rand() % 200;
+double numGeneratorY(){
+    double yCoor = rand() % 200;
     return yCoor;
 }
 
 int main() {
-  int xCoor = numGeneratorX();
-  int yCoor = numGeneratorY();
+  //double xCoor = numGeneratorX();
+  //int yCoor = numGeneratorY();
 
-  int gd = DETECT, gm;
+  int square[200][200];
+  int radius = 100;
 
-  //location of the left, top, right, and bottom of square
-  int left = 0, top = 0;
-  int right = 200, bottom = 200;
-  //location of center and radius of circle
-  int x = 100, y = 100, radius = 100;
 
-  //initializes graph
-  initgraph(&gd, &gm, "");
+  double pi = (4 * insideCount) / totalCount;
+  printf("The estimated value of Pi is %f \n", pi);
 
-  //creates rectangle and cirle
-  rectangle(left, top, right, bottom);
-  circle(x, y, radius);
 
-  getch();
-  closegraph();
 
   return 0;
-
-
-
-  //return pi = 4 x (number of points in the circle/total number of points)
-
 }
 
-//void function that grabs generated X and Y vals and return 2 coordinates
+void pointGenerator(){
+  double xCoor = numGeneratorX();
+  double yCoor = numGeneratorY();
+
+  if (sqrt(((xCoor - circleX)*(xCoor - circleX) + ((yCoor - circleY)*(yCoor - circleY))) < 100)) {
+    totalCount++;
+    insideCount++;
+  }
+  else {
+    totalCount++;
+  }
+  return;
+}
+
 
 //function to check whether point is in cirlce or not
   //if point is greater than radius, it is outside the circle
--- Sudoku_Checker.c
+++ Sudoku_Checker.c
@@ -1,17 +1,13 @@
 //#include <iostream.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <stdio.h>
+
 
 //gcc -o sdk Sudoku_Checker.c
 
 #define NUM_THREADS 9
 
-typedef struct
-{
-  int col;
-  int row;
-  int(*board)[9];
-} parameters;
 
 typedef int bool;
 #define true 1
@@ -42,73 +38,163 @@ int failBoard[9][9] ={
 };
 
 //pthread_create (thread, attribute, start_routine, argument)
-void *colChecker(void *param);
-void *rowChecker(void *param);
-void remove_element(int array[], int index, int array_length);
-int tbtChecker(int array[][], int x, int y);
+int colChecker(int array[9][9]);
+int rowChecker(int array[9][9]);
+int tbtchecker(int array[9][9], int x, int y);
 
 //main process thread
 int main(int argc, char const *argv[]) {
+
+  /* the threads IDs */
+  pthread_t thread_cols, thread_rows, thread_tbt1, thread_tbt2, thread_tbt3,
+    thread_tbt4, thread_tbt5, thread_tbt6, thread_tbt7, thread_tbt8, thread_tbt9;
+
+  /* Initializes the threads */
+  pthread_create(&thread_cols, NULL, colChecker, );
+  pthread_create(&thread_rows, NULL, rowChecker, );
+  pthread_create(&thread_tbt1, NULL, tbtchecker, );
+  pthread_create(&thread_tbt2, NULL, tbtchecker, );
+  pthread_create(&thread_tbt3, NULL, tbtchecker, );
+  pthread_create(&thread_tbt4, NULL, tbtchecker, );
+  pthread_create(&thread_tbt5, NULL, tbtchecker, );
+  pthread_create(&thread_tbt6, NULL, tbtchecker, );
+  pthread_create(&thread_tbt7, NULL, tbtchecker, );
+  pthread_create(&thread_tbt8, NULL, tbtchecker, );
+  pthread_create(&thread_tbt9, NULL, tbtchecker, );
+
+
+  /* Joins the threads and waits for them to finish executing */
+  pthread_join(thread_cols, );
+  pthread_join(thread_rows);
+  pthread_join(thread_tbt1);
+  pthread_join(thread_tbt2);
+  pthread_join(thread_tbt3);
+  pthread_join(thread_tbt4);
+  pthread_join(thread_tbt5);
+  pthread_join(thread_tbt6);
+  pthread_join(thread_tbt7);
+  pthread_join(thread_tbt8);
+  pthread_join(thread_tbt9);
+
+
   /* code */
+  int hi = rowChecker(successBoard);
+  int x = tbtchecker(successBoard, 0, 0);
+  int a = tbtchecker(successBoard, 3, 0);
+  int b = tbtchecker(successBoard, 6, 0);
+  int c = tbtchecker(successBoard, 0, 3);
+  int d = tbtchecker(successBoard, 0, 6);
+  int e = tbtchecker(successBoard, 3, 3);
+  int f = tbtchecker(successBoard, 3, 6);
+  int g = tbtchecker(successBoard, 6, 6);
+  int h = tbtchecker(successBoard, 6, 3);
+
   return 0;
 }
 
 //checks columns for digits 1 through 9
-void *colChecker(void *param) {
+int colChecker(int array[9][9]) {
 
+    for(int i = 0; i < 9; i++)
+    {
+      int count = 0;
+      int num[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+      for(int j = 0; j < 9; j++)
+      {
+        for(int k = 0; k < 9; k++)
+        {
+          if(j == 8)
+          {
+            if(array[i][j] == num[k])
+            {
+              count++;
+              num[k] = 0;
+              if(count == 9)
+              {
+                continue;
+              }
+              else
+              {
+                return 0;
+              }
+            }
+          }
+          if(array[i][j] == num[k])
+          {
+            count++;
+            num[k] = 0;
+            break;
+          }
+      }
+    }
+}
+    return 1;
 }
 
-void *rowChecker(void *param) {
-
-  int num[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int length = 9;
-  int count = 0;
+int rowChecker(int array[9][9]) {
   for(int i = 0; i < 9; i++)
   {
-    for(int j = 0; j<9; j++)
+    int count = 0;
+    int num[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for(int j = 0; j < 9; j++)
     {
-
+      for(int k = 0; k < 9; k++)
+      {
+        if(j == 8)
+        {
+          if(array[j][i] == num[k])
+          {
+            count++;
+            num[k] = 0;
+            if(count == 9)
+            {
+              continue;
+            }
+            else
+            {
+              return 0;
+            }
+          }
+        }
+        if(array[j][i] == num[k])
+        {
+          count++;
+          num[k] = 0;
+          break;
+        }
     }
   }
-
+}
+  return 1;
 }
 
-int tbtchecker(int array[][], int x, int y){
+int tbtchecker(int array[9][9], int x, int y){
+
   int num[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int length = 9;
   int count = 0;
   for (int i = x; i < x+3; i++) {
-    if(count == 9)
-    {return 1;}
-    for (int j = y; j < j+3; j++) {
-      for(int k = 1; k < 10; k++)
+    for (int j = y; j < y+3; j++) {
+      for(int k = 0; k < 9; k++)
       {
         if(array[i][j] == num[k])
         {
-          remove_element(num, k , length);
-          count++;
+          num[k] = 0;
           break;
         }
       }
     }
   }
+  for(int i = 0; i < 9; i++)
+  {
+    if(num[i] == 0)
+    {
+      count++;
+    }
+  }
+  if(count == 9)
+  {
+    return 1;
+  }
+
   return 0;
 }
-
-void remove_element(int array[], int index, int array_length)
-{
-  for(int i = index; i < array_length; i++)
-  {
-    array[i] = array[i+1];
-  }
-  int *tmp = realloc(array, (array_length - 1)* sizeof(array));
-  if(tmp == NULL && array_length > 1)
-  {
-    exit(EXIT_FAILURE);
-  }
-array_length = array_length - 1;
-array = tmp;
-
-}
-
-pthread_exit();
