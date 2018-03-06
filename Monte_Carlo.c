a9513c28f703049b49e400641b61a117229bd587
Kathleen Elisabeth Smith <smithka19@up.edu>
2018-03-05 17:03:14 -0800

    monte

--- Monte_Carlo.c
+++ Monte_Carlo.c
@@ -1,7 +1,10 @@
 //#include <iostream>
-#include <cstdlib>
+#include <stdlib.h>
+#include <stdio.h>
 #include <pthread.h>
-#include <graphics.h>
+#include "graphics.h"
+#include "winbgim.h"
+//#include <conio.h>
 #include <time.h>
 
 //gcc -o mc Monte_Carlo.c
@@ -9,28 +12,47 @@
 //PUT DEFINITIONS HERE
 
 //SQUARE IS 200X200
+
+int numGeneratorX(){
+  srand(time(0));
+  int xCoor = rand() % 200;
+  return xCoor;
+}
+
+int numGeneratorY(){
+    int yCoor = rand() % 200;
+    return yCoor;
+}
+
 int main() {
+  int xCoor = numGeneratorX();
+  int yCoor = numGeneratorY();
+
+  int gd = DETECT, gm;
+
+  //location of the left, top, right, and bottom of square
+  int left = 0, top = 0;
+  int right = 200, bottom = 200;
+  //location of center and radius of circle
+  int x = 100, y = 100, radius = 100;
+
+  //initializes graph
+  initgraph(&gd, &gm, "");
+
+  //creates rectangle and cirle
+  rectangle(left, top, right, bottom);
+  circle(x, y, radius);
+
+  getch();
+  closegraph();
+
+  return 0;
 
 
-//return pi = 4 x (number of points in the circle/total number of points)
+
+  //return pi = 4 x (number of points in the circle/total number of points)
 
 }
-
-int *numGeneratorX (void){
-  int i,n;
-  srand(time(0));
-
-  for (int i; i < n+1; i++)  {
-    printf("%d\n", rand() % 200);
-}
-int *numGeneratorY (void){
-    int i,n;
-}
-  srand(time(0));
-
-  for (int i; i < n+1; i++)  {
-    printf("%d\n", rand() % 200);
-  }
 
 //void function that grabs generated X and Y vals and return 2 coordinates
 
